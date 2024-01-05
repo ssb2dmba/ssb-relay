f7b896008f2af09b670c5a0ac075767ea42ab2d2
dmba <eflorent@tbprl>
2024-01-05 09:48:56 +0100

    fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type {Config} from '@jest/types';
 // Sync object
 const config: Config.InitialOptions = {
+  globalSetup: "./jestGlobalSetup.js",
   verbose: true,
   transform: {
   "^.+\\.tsx?$": "ts-jest",
--- src/use-cases/auth/jwt-adapter.ts
+++ src/use-cases/auth/jwt-adapter.ts
@@ -10,6 +10,7 @@ export class JwtAdapter  {
 
 
     static async encrypt(text: string | number | Buffer): Promise<any> {
+
         const md5fn = (contents: string) => crypto.createHash('md5').update(contents).digest("hex");
         const secret = md5fn( readFileSync(join(homedir(),".ssb", "secret"), 'utf-8'));
         return jwt.sign({account: text}, secret, {expiresIn: "1d"});
