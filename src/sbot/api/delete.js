e9509c4eec4045dbae415c7a01ce916a12491750
dmba <eflorent@tbprl>
2024-05-04 15:01:06 +0200

    feat: post-delete

---
+++ src/sbot/api/delete.js
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the ssb-postgres distribution (https://github.com/ssb2dmba/ssb-postgres).
+ * Copyright (c) 2023 DMBA Emmanuel Florent.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module.exports = function implementation(db) {
+
+    async function deleteMessage(pool, key) {
+        const text = "delete from message where message->>'key' = ($1)";
+        return await pool.query(text, [key]);
+    }
+
+
+    db.delete = async function (key, cb) {
+
+        try {
+            let data = await deleteMessage(db.pool, key);
+            console.log(data)
+            if (data.rowCount == 1) {
+                cb(null, data)
+            } else {
+                cb(null, {})
+            }
+        } catch (e) {
+            cb(e, null)
+        }
+    }
+    return db;
+
+}
